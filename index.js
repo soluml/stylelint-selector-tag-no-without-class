bb9ee137248d8fecbfc6829716402b4676ab59aa
soluml <benjamin@soluml.com>
2023-07-01 10:48:50 -0500

    Still a few issues

--- index.js
+++ index.js
@@ -1,88 +1,120 @@
-const _ = require('lodash');
-const stylelint = require('stylelint');
+const _ = require("lodash");
+const stylelint = require("stylelint");
 const isStandardSyntaxRule = require("stylelint/lib/utils/isStandardSyntaxRule");
 const isStandardSyntaxSelector = require("stylelint/lib/utils/isStandardSyntaxSelector");
 const parseSelector = require("stylelint/lib/utils/parseSelector");
 const matchesStringOrRegExp = require("stylelint/lib/utils/matchesStringOrRegExp");
 
-const ruleName = 'plugin/selector-tag-no-without-class';
+const ruleName = "plugin/selector-tag-no-without-class";
 const messages = stylelint.utils.ruleMessages(ruleName, {
-	unexpected: (tagName) => `Unexpected tag ${tagName} without class qualifier`
+  unexpected: (tagName) =>
+    `Unexpected top-level tag ${tagName} without class qualifier`,
 });
 
-const rule = function(primaryOption) {
-	return function(root, result) {
-		let validOptions = stylelint.utils.validateOptions(result, ruleName, {
-			actual: primaryOption,
-			possible: [_.isString]
-		});
-		if (!validOptions) {
-			return;
-		}
+const rule = function (primaryOption) {
+  return function (root, result) {
+    let validOptions = stylelint.utils.validateOptions(result, ruleName, {
+      actual: primaryOption,
+      possible: [_.isString],
+    });
 
-		function checkSelector(selectorNode, ruleNode) {
-			let combinedSegments = selectorNode.split(node => {
-				return node.type === 'combinator';
-			});
+    if (!validOptions) {
+      return;
+    }
 
-			combinedSegments.forEach(segment => {
-				let unqualifiedTagNode;
-				segment.forEach(node => {
-					if (node.type === 'tag' && matchesStringOrRegExp(node.value, primaryOption)) {
-						unqualifiedTagNode = node;
-					}
-					if (node.type === 'class') {
-						unqualifiedTagNode = void 0;
-					}
-				});
+    function checkSelector(selectorNode, ruleNode) {
+      const combinedSegments = selectorNode.split(
+        (node) => node.type === "combinator"
+      );
+      const startsWith = combinedSegments.flat(Infinity)[0];
 
-				if (unqualifiedTagNode) {
-					stylelint.utils.report({
-						ruleName: ruleName,
-						result: result,
-						node: ruleNode,
-						message: messages.unexpected(unqualifiedTagNode.value),
-						word: unqualifiedTagNode
-					});
-				}
-			});
-		}
+      if (startsWith.type === "tag") {
+        stylelint.utils.report({
+          ruleName: ruleName,
+          result: result,
+          node: ruleNode,
+          message: messages.unexpected(startsWith.value),
+          word: startsWith,
+        });
+      }
 
-		function checkSelectorRoot(selectorRootNode, ruleNode) {
-			selectorRootNode.each(selectorNode => {
-				checkSelector(selectorNode, ruleNode);
-			});
-		}
+      // combinedSegments.forEach((segment) => {
+      //   let unqualifiedTagNode;
+      //   segment.forEach((node) => {
+      //     if (
+      //       node.type === "tag" &&
+      //       matchesStringOrRegExp(node.value, primaryOption)
+      //     ) {
+      //       unqualifiedTagNode = node;
+      //     }
+      //     if (node.type === "class") {
+      //       unqualifiedTagNode = void 0;
+      //     }
+      //   });
 
-		root.walkRules(ruleNode => {
-			if (!isStandardSyntaxRule(ruleNode)) {
-				return;
-			}
-			if (!isStandardSyntaxSelector(ruleNode.selector)) {
-				return;
-			}
-			if (
-				ruleNode.nodes.some(
-					node => ["rule", "atrule"].indexOf(node.type) !== -1
-				)
-			) {
-				// Skip unresolved nested selectors
-				return;
-			}
-			if (ruleNode.parent && ruleNode.parent.type === "atrule" && ruleNode.parent.name === "keyframes") {
-				// Skip rules within an @keyframes at-rule
-				// (https://github.com/Moxio/stylelint-selector-tag-no-without-class/issues/5)
-				return;
-			}
+      //   if (unqualifiedTagNode) {
+      //     stylelint.utils.report({
+      //       ruleName: ruleName,
+      //       result: result,
+      //       node: ruleNode,
+      //       message: messages.unexpected(unqualifiedTagNode.value),
+      //       word: unqualifiedTagNode,
+      //     });
+      //   }
+      // });
+    }
 
-			ruleNode.selectors.forEach(selector => {
-				parseSelector(selector, result, ruleNode, container =>
-					checkSelectorRoot(container, ruleNode)
-				);
-			});
-		});
-	};
+    function checkSelectorRoot(selectorRootNode, ruleNode) {
+      selectorRootNode.each((selectorNode) => {
+        checkSelector(selectorNode, ruleNode);
+      });
+    }
+
+    root.walkRules((ruleNode) => {
+      if (!isStandardSyntaxRule(ruleNode)) {
+        return;
+      }
+
+      if (!isStandardSyntaxSelector(ruleNode.selector)) {
+        return;
+      }
+
+      if (
+        ruleNode.nodes.some(
+          (node) => ["rule", "atrule"].indexOf(node.type) !== -1
+        )
+      ) {
+        // Skip unresolved nested selectors
+        return;
+      }
+
+      if (
+        ruleNode.parent &&
+        ruleNode.parent.type === "atrule" &&
+        ruleNode.parent.name === "keyframes"
+      ) {
+        // Skip rules within an @keyframes at-rule
+        // (https://github.com/Moxio/stylelint-selector-tag-no-without-class/issues/5)
+        return;
+      }
+
+      console.log(`=============
+${ruleNode.toString()}
+=============`);
+
+      ruleNode.selectors.forEach((selector) => {
+        parseSelector(selector, result, ruleNode, (container) =>
+          checkSelectorRoot(container, ruleNode)
+        );
+      });
+
+      console.log(`+++++++++++++
++++++++++++++
++++++++++++++`);
+    });
+  };
 };
+
 rule.primaryOptionArray = true;
 rule.ruleName = ruleName;
 rule.messages = messages;
--- index.test.js
+++ index.test.js
@@ -1,64 +1,76 @@
 const {
-	rule: { ruleName, messages }
-} = require('.');
+  rule: { ruleName, messages },
+} = require(".");
 
 testRule({
-	ruleName: ruleName,
-	config: [ 'span', 'div' ],
+  ruleName: ruleName,
+  config: ["span"],
 
-	accept: [
-		{ code: 'span.foo, h1.bar > h2.baz > h3.qux {}' },
-		{ code: 'span.foo {}' },
-		{ code: 'div.foo {}' },
-		{ code: 'h1 {}' },
-	],
+  accept: [
+    // { code: "span.foo, h1.bar > h2.baz > h3.qux {}" },
+    // { code: "span.foo {}" },
+    // { code: "div.foo {}" },
 
-	reject: [
-		{
-			code: 'div {}',
-			message: messages.unexpected('div'),
-			line: 1,
-			column: 1
-		},
-		{
-			code: 'div, div.foo {}',
-			message: messages.unexpected('div'),
-			line: 1,
-			column: 1
-		},
-		{
-			code: '.foo div {}',
-			message: messages.unexpected('div'),
-			line: 1,
-			column: 6
-		},
-		{
-			code: 'div:hover {}',
-			message: messages.unexpected('div'),
-			line: 1,
-			column: 1
-		},
-		{
-			code: 'div::before {}',
-			message: messages.unexpected('div'),
-			line: 1,
-			column: 1
-		},
-		{
-			code: 'div:first-child {}',
-			message: messages.unexpected('div'),
-			line: 1,
-			column: 1
-		},
-	]
+    { code: ".foo span {}" },
+    { code: ".foo { > span {} }" },
+    {
+      code: "@media(min-width: 30em) { @media(min-width: 40em) { .foo span {} } }",
+    },
+  ],
+
+  reject: [
+    { code: "h1 {}", message: messages.unexpected("h1"), line: 1, column: 1 },
+    {
+      code: "div {}",
+      message: messages.unexpected("div"),
+      line: 1,
+      column: 1,
+    },
+    {
+      code: "@media(min-width: 30em) { @media(min-width: 40em) { span {} } }",
+      message: messages.unexpected("span"),
+      line: 1,
+      column: 53,
+    },
+    {
+      code: "div:hover {}",
+      message: messages.unexpected("div"),
+      line: 1,
+      column: 1,
+    },
+    {
+      code: "div::before {}",
+      message: messages.unexpected("div"),
+      line: 1,
+      column: 1,
+    },
+    {
+      code: "div:first-child {}",
+      message: messages.unexpected("div"),
+      line: 1,
+      column: 1,
+    },
+    // {
+    //   code: "div, div.foo {}",
+    //   message: messages.unexpected("div"),
+    //   line: 1,
+    //   column: 1,
+    // },
+    // {
+    //   code: ".foo div {}",
+    //   message: messages.unexpected("div"),
+    //   line: 1,
+    //   column: 6,
+    // },
+  ],
 });
 
-testRule({
-	ruleName: ruleName,
-	config: [ '/./' ],
+// testRule({
+//   ruleName: ruleName,
+//   config: ["/./"],
 
-	accept: [
-		// https://github.com/Moxio/stylelint-selector-tag-no-without-class/issues/5
-		{ code: '@keyframes fade-in { from { opacity: 0; } to { opacity: 1; } }' },
-	]
-});
+//   accept: [
+//     // https://github.com/Moxio/stylelint-selector-tag-no-without-class/issues/5
+//     { code: "@keyframes fade-in { from { opacity: 0; } to { opacity: 1; } }" },
+//   ],
+// });
